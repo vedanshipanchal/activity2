1d6ae7e5aac21556ae6e2201c1e68f206516152e
vedanshipanchal <vedanshipanchal@gmail.com>
2024-09-06 17:56:01 +0100

    activity 2

---
+++ main.py
@@ -0,0 +1,15 @@
+#type casting
+name = "codingal"
+age = 13
+rating = 9.7
+education = True
+#types
+print(type(name))
+print(type(age))
+print(type(rating))
+print(type(education))
+#type casting means type conversion
+
+#convert float to int(converted point value to without point)
+rating = int(rating)
+print(rating)
